12268c0d251d4e15335ed8c287caf93dc79d16b6
Veysel Aksin <32255510+veyselaksin@users.noreply.github.com>
2023-10-04 17:49:38 +0300

    portfolio-app-v1 about section created
    portfolio-app-v1 about section created

--- portfolio-v1/src/components/about/About.jsx
+++ portfolio-v1/src/components/about/About.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
 
 function About() {
-    return <section id='about'>About</section>
+    return (
+        <section id='about'>
+            <h5>Get To Know</h5>
+            <h2>About Me</h2>
+
+            <div className='container about-container'>
+                <div className='about-me'>
+                    <div className='about-me-image'>
+                        <img src={ME} alt='About Me Image' />
+                    </div>
+                </div>
+
+                {/* <div className='about-content'> */}
+            </div>
+        </section>
+    )
 }
 
 export default About
