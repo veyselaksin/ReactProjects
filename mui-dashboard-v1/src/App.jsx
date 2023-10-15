ee5577dcb413c550a7a486a261b0d5efc25da96e
Veysel Aksin <32255510+veyselaksin@users.noreply.github.com>
2023-10-16 00:28:28 +0300

    Edit Dashboard Themes
    Edit Dashboard Themes

--- mui-dashboard-v1/src/App.jsx
+++ mui-dashboard-v1/src/App.jsx
@@ -1,8 +1,22 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./components/Topbar";
+
 import "./App.css";
-import Dashboard from "./pages/dashboard";
 
 function App() {
-  return <></>;
+  const [theme, colorMode] = useMode();
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <div className="app">
+          <main className="content">
+            <Topbar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
 export default App;
